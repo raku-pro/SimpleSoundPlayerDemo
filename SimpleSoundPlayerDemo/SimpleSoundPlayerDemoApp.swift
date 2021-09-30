e08dd7e864f0e8ee62f98c0a11ee5ead973c38b3
rakupro <rakuprog@gmail.com>
2021-09-30 14:17:33 +0900

    Initial Commit

---
+++ SimpleSoundPlayerDemo/SimpleSoundPlayerDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleSoundPlayerDemoApp.swift
+//  SimpleSoundPlayerDemo
+//
+//  Created by XcodeDeveloper on 2021/09/30.
+//
+
+import SwiftUI
+
+@main
+struct SimpleSoundPlayerDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
